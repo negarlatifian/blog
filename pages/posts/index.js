85cbf9d94d0fb5de64f0662f9aed4a1ff69df34f
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-10-27 18:34:36 +0100

    allPosts page created with dummy data

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,41 @@
-import React from "react";
+import AllPosts from "../../components/posts/all-posts";
 
 const AllPostsPage = () => {
-  return <div>AllPostsPage</div>;
+  const DUMMY_POSTS = [
+    {
+      slug: "getting-started-with-nextjs",
+      title: "Getting Started with NextJS",
+      excerpt:
+        "NextJS is a Rect framework for production. It makes building fullstack React apps and sites a breeze and ships with Server Side Rendering(SSR)",
+      image: "getting-started-nextjs.png",
+      date: "2022-10-27",
+    },
+    {
+      slug: "getting-started-with-nextjs2",
+      title: "Getting Started with NextJS",
+      excerpt:
+        "NextJS is a Rect framework for production. It makes building fullstack React apps and sites a breeze and ships with Server Side Rendering(SSR)",
+      image: "getting-started-nextjs.png",
+      date: "2022-10-27",
+    },
+    {
+      slug: "getting-started-with-nextjs3",
+      title: "Getting Started with NextJS",
+      excerpt:
+        "NextJS is a Rect framework for production. It makes building fullstack React apps and sites a breeze and ships with Server Side Rendering(SSR)",
+      image: "getting-started-nextjs.png",
+      date: "2022-10-27",
+    },
+    {
+      slug: "getting-started-with-nextjs4",
+      title: "Getting Started with NextJS",
+      excerpt:
+        "NextJS is a Rect framework for production. It makes building fullstack React apps and sites a breeze and ships with Server Side Rendering(SSR)",
+      image: "getting-started-nextjs.png",
+      date: "2022-10-27",
+    },
+  ];
+  return <AllPosts posts={DUMMY_POSTS} />;
 };
 
 export default AllPostsPage;
