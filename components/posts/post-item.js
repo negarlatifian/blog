c833629e39d064122821f64a88b0a1f4579754d2
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-10-27 17:43:56 +0100

    post-item component created

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,5 +1,34 @@
-const PostItem = () => {
-  return <div>PostItem</div>;
+import Image from "next/image";
+import Link from "next/link";
+import classes from "./post-item.module.css";
+
+const PostItem = (props) => {
+  const { title, image, exerpt, date, slug } = props.post;
+
+  const formattedDate = new Date(date).toLocaleString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+
+  return (
+    <li className={classes.post}>
+      <Link href={slug}>
+        <a>
+          <div className={classes.image}>
+            <Image src={imagePath} alt={title} width={300} height={200} />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <time>{date}</time>
+            <p>{exerpt}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
 };
 
 export default PostItem;
