75e058a4853fabcc77931c5501c5c0da39624215
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-10-30 18:57:29 +0100

    added markdown to postDetail page

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,19 +3,13 @@ import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting Started with NextJS",
-  date: "2022-10-27",
-  image: "getting-started-nextjs.png",
-  content: "# This is the first post",
-};
-const PostContent = () => {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+const PostContent = (props) => {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 };
