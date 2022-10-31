ab09958b54667a884371b754e4b5ba8b258f11db
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-10-31 21:51:51 +0100

    added _document file

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+class MyDocument extends Document {
+  render() {
+    return (
+      <html lang='en'>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+export default MyDocument;
