543f404f0202467ff025bd7a93403346cb70506e
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-10-31 14:56:58 +0100

    rendered image in a div using components prop

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -8,15 +8,32 @@ const PostContent = (props) => {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
   const customComponents = {
-    img: (image) => {
-      return (
-        <Image
-          src={`/images/posts/${post.slug}/${image.src}`}
-          alt={image.alt}
-          width={600}
-          height={300}
-        />
-      );
+    // img: (image) => {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p: (paragraph) => {
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
     },
   };
   return (
